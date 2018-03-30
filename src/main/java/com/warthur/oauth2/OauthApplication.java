642be8ac9362b063fc0021fcdab2b9edde7cc2a9
warthur <warthur@163.com>
2018-03-30 17:46:18 +0800

    spring oauth2 code

---
+++ src/main/java/com/warthur/oauth2/OauthApplication.java
@@ -0,0 +1,15 @@
+package com.warthur.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by warth on 2018/3/30.
+ */
+@SpringBootApplication
+public class OauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthApplication.class, args);
+	}
+}
